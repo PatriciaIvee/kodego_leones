7b5fc91ddd15bf8c9a5c0634f69330585e272b1e
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-10-07 21:16:01 +0800

    Created activity_01_a made assignment activity 01 A from Canvas

---
+++ src/main/kotlin/activity_01_a/Main.kt
@@ -0,0 +1,46 @@
+package activity_01_a
+
+import java.sql.Time
+import java.time.Year
+import java.util.Date
+
+fun main() {
+    //People involved in the directory
+    var student : String
+    var instructor : String
+    var instructorAssistant : String
+
+    //details about your class
+    var classBatch : String
+    var course : String
+    var subject: String
+    var classSchedule : Date
+    var roomNumber : Int
+    var roomName: String
+    var timeIn : Time
+    var timeOut : Time
+    var classDuration : Time
+    var absent : Boolean
+    var late : Boolean
+    var year: Year
+    var tuitionStatus : String
+
+    //materials, announcements etc.
+    var announcements: String
+    var module: String
+    var exercise : String
+    var assignment : String
+    var quiz : String
+    var project : String
+    var recording: String
+    var grade : Boolean
+    var score : Int
+    var completed : Boolean
+    var totalModules : Int
+    var totalExercises: Int
+    var totalAssignments: Int
+    var totalQuizzes: Int
+    var totalProjects: Int
+    var totalRecordings: Int
+
+}
