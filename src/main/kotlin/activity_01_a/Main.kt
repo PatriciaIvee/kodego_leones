0e3c23bab4fe35b84b3e5ba2cb7bc773a1611faa
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-10-07 23:04:16 +0800

    Created activity_01_a made assignment activity 01 A from Canvas Updated other variables and values needed for School Directory

--- src/main/kotlin/activity_01_a/Main.kt
+++ src/main/kotlin/activity_01_a/Main.kt
@@ -6,41 +6,45 @@ import java.util.Date
 
 fun main() {
     //People involved in the directory
+    val schoolDirectory : String
     var student : String
     var instructor : String
     var instructorAssistant : String
 
     //details about your class
+    val classDetails : String
     var classBatch : String
     var course : String
-    var subject: String
+    var subject : String
     var classSchedule : Date
     var roomNumber : Int
-    var roomName: String
+    var roomName : String
     var timeIn : Time
     var timeOut : Time
     var classDuration : Time
     var absent : Boolean
     var late : Boolean
-    var year: Year
+    var year : Year
     var tuitionStatus : String
 
-    //materials, announcements etc.
+    //Information to complete and pass the course, materials, announcements etc.
+    val classInformation : String
     var announcements: String
     var module: String
     var exercise : String
     var assignment : String
     var quiz : String
     var project : String
-    var recording: String
+    var recording : String
     var grade : Boolean
     var score : Int
     var completed : Boolean
     var totalModules : Int
-    var totalExercises: Int
-    var totalAssignments: Int
-    var totalQuizzes: Int
-    var totalProjects: Int
-    var totalRecordings: Int
-
+    var totalExercises : Int
+    var totalAssignments : Int
+    var totalQuizzes : Int
+    var totalProjects : Int
+    var totalRecordings : Int
+    var totalScore : Int
+    var event : String
 }
