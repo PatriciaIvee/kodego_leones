e577907e61ae02648d620e28bb9dea120ab65012
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-10-08 19:42:08 +0800

    Created package activity_01_b and Main.kt for Activity 01 b on Canvas

---
+++ src/main/kotlin/activity_01_b/Main.kt
@@ -0,0 +1,59 @@
+package activity_01_b
+
+import java.time.format.DateTimeFormatter
+import java.util.Date
+
+fun main() {
+//    DateFormatter
+    var dateFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy")
+//    School Library Directory label
+    val schoolLibraryDirectory : String
+
+//    Status if item is borrowed or available
+    var borrowed : Boolean = false
+    var available : Boolean = false
+    var dateBorrowed: Date
+    var returnDate : Date
+    var returned : Boolean = false
+    var lost : Boolean = false
+
+//    Information about book or ebook
+    var eBookTitle : String
+    var bookTitle : String
+    var author : String
+    var bookId : String
+    var isbnBookNumber : Long
+    var eIsbnBookNumber : Long
+    var publisher : String?
+    var publishedDate : Date?
+    var edition : String?
+    var numberOfPages : Int?
+    var numberOfCopies : Int?
+    var shelfNumber : Int
+    var copyright : String
+    var subject : String?
+    var classification : String
+    var language : String
+    var eBookFileSize : Int
+    var totalBorrowedBooks: Int?
+    var borrowHistory : Date
+
+
+//  Information about items borrowed in a library that are not books
+    var musicResource : String
+    var itemIdNumber : String
+    var electronicEquipmentResource : String
+    var newsArticleResource : String
+    var mapResource : String
+    var visualMaterialResource : String
+    var type : String
+    var itemTotal: Int
+
+
+//    Sorting Information
+    var resourceType : String
+    var alphabeticalOrder : String
+    var ascendingOrder : Boolean
+    var descendingOrder : Boolean
+
+}
