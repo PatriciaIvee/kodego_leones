34dc6051c5257e3180e993b24de0957472ba2603
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-10-08 14:59:26 +0800

    added nullable variables.

--- src/main/kotlin/activity_01_a/Main.kt
+++ src/main/kotlin/activity_01_a/Main.kt
@@ -2,22 +2,42 @@ package activity_01_a
 
 import java.sql.Time
 import java.time.Year
+import java.time.format.DateTimeFormatter
 import java.util.Date
 
 fun main() {
+    //dateFormat
+    val dateFormat =  DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm")
     //People involved in the directory
     val schoolDirectory : String
     var student : String
     var instructor : String
     var instructorAssistant : String
 
+    //Student or instructor information
+    var lastName : String
+    var firstName : String
+    var middleName : String
+    var suffix : Any?
+    var birthDate = Date()
+    var age = 0 .. 100
+    var gender : String
+    var address: String
+    var city : String
+    var province : String
+    var zipCode : Int
+    var landlineNumber : Int?
+    var cellPhone: Long
+    var email : String?
+    var department : String?
+
     //Details about your course and class
-    var courseStart : Date
-    var courseEnd : Date
+    var classStart : Date
+    var classEnd : Date
     val classDetails : String
-    var classBatch : String
-    var course : String
-    var subject : String
+    var classBatch : String?
+    var course : String?
+    var subject : String?
     var classSchedule : Date
     var roomNumber : Int
     var roomName : String
@@ -27,19 +47,20 @@ fun main() {
     var absent : Boolean
     var late : Boolean
     var year : Year
+    var paid : Boolean
     var tuitionStatus : String
 
     //Information to complete and pass the course, materials, announcements etc.
     val classInformation : String
-    var announcements: String
-    var module: String
-    var exercise : String
-    var assignment : String
-    var quiz : String
-    var project : String
-    var recording : String
+    var announcements: String?
+    var module: String?
+    var exercise : String?
+    var assignment : String?
+    var quiz : String?
+    var project : String?
+    var recording : String?
     var grade : Boolean
-    var score : Int
+    var score : Int?
     var completed : Boolean
     var totalModules : Int
     var totalExercises : Int
