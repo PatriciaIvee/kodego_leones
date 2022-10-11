8f3a76628e53ea5de11433f59ff92486a56d6a5a
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-10-11 17:43:45 +0800

    Added more notes for Lecture 10-10-2022

--- src/main/kotlin/UnaryOperation.kt
+++ src/main/kotlin/UnaryOperation.kt
@@ -4,13 +4,37 @@ fun main() {
     var plusPlus = 0
     var minusMinus = 0
 
-//    increment + 1
-    increment += 1
+    //    increment + 1
+    //    increment += 1
 
-//    decrement - 1
-    increment -= 1
+    //    decrement - 1
+    //    increment -= 1
 
-//    plusPlus = ++
-//    minusMinus = --
+    //    plusPlus = ++
+    //    minusMinus = --
 
+    increment = 2
+    println("Increment : $increment")
+    println(++ increment)
+    //   by adding plusplus(increment) before the var, the value of var will be incremented by 1
+    println(increment ++)
+    /* by adding the plusplus (increment) after the var,the value of var
+    will be returned first then it will be incremented by 1
+     */
+
+    decrement = 2
+    println("Decrement : $decrement")
+
+    println(--decrement)
+    //   by adding plusplus(increment) before the var, the value of var will be incremented by 1
+    println(decrement --)
+    /* by adding the minusminus (decrement) after the var,the value of var
+    will be returned first then it will be incremented by 1
+     */
+
+    increment += 2
+    println("Increment : $increment")
+
+    decrement -= 2
+    println("Increment : $decrement")
 }
