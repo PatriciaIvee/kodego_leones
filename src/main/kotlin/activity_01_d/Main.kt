1c9ee39ac0112942dd6bcbf9be53318b54502b12
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-10-11 19:23:22 +0800

    updated code with !! in variables Activity 01 d in Canvas

--- src/main/kotlin/activity_01_d/Main.kt
+++ src/main/kotlin/activity_01_d/Main.kt
@@ -12,24 +12,45 @@ fun main() {
     if (ready.uppercase() == "Y" ) {
         println("Okay let's proceed enter the first number.")
         println(enterHere)
-        var num1 : String? = readLine().toString()
+
+//        var num1 = readLine()!!.toDouble()
+        var num1 = readLine().toDouble()
+//        var num1Double = num1.toDouble()
         println("You entered $num1. Great! Now enter the second number.")
         println(enterHere)
-        var num2 : String? = readLine().toString()
+
+//        var num2 = readLine()!!.toDouble()
+        var num2 = readLine().toDouble()
+//        var num2Double = num2.toDouble()
         println("You entered $num2. Great! Now enter the third number.")
         println(enterHere)
-        var num3 : String? = readLine().toString()
+
+//        var num3 = readLine()!!.toDouble()
+        var num3 = readLine()!!.toDouble()
+//        var num3Double = num3.toDouble()
         println("You entered $num3. Great! Now enter the fourth number.")
         println(enterHere)
-        var num4 : String? = readLine().toString()
+
+//        var num4 : Double = readLine()!!.toDouble()
+        var num4 : Double = readLine().toDouble()
+//        var num4Double = num4.toDouble()
         println("You entered $num4. Great! Now enter the fifth number.")
         println(enterHere)
-        var num5 : String? = readLine().toString()
+
+//        var num5 : Double = readLine()!!.toDouble()
+        var num5 : Double = readLine()!!.toDouble()
+//        var num5Double = num5.toDouble()
         println("You entered $num5. Okay! Now enter a number to divide all numbers to.")
         println(enterHere)
-        var divisor : String? = readLine().toString()
+
+//        var divisor : Double = readLine()!!.toDouble()
+        var divisor : Double = readLine().toDouble()
+//        var divisorDouble = divisor.toDouble()
+
         println("Okay the answer is...")
-        println((num1 + num2 + num3 +num4 + num5)/divisor.toInt())
+//        println(((num1Double + num2Double + num3Double +num4Double + num5Double)/divisorDouble).equals(Int))
+        println((num1+num2+num3+num4+num5).toInt()/divisor.toInt())
+
     } else if (ready.uppercase() == "N") {
         println("Okay till next time!")
     } else {
