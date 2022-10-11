3d54f1b8988d1cd8a86c3a67499e2ba96967b47c
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-10-11 20:32:00 +0800

    added peso sign to print lines

--- src/main/kotlin/activity_01_d/Main.kt
+++ src/main/kotlin/activity_01_d/Main.kt
@@ -4,7 +4,7 @@ fun main() {
     val applicationName : String = "Welcome to Exercise activity_01_d "
     println(applicationName)
 
-    var inputInstruction : String = "I will ask you to input 5 amounts are you ready? (Y/N)"
+    var inputInstruction : String = "I will ask you to input 5 monetary amounts are you ready? (Y/N)"
     println(inputInstruction)
     var enterHere : String = "Enter number : "
 
@@ -16,13 +16,13 @@ fun main() {
        var num1 = readLine()!!.toDouble()
 //        var num1 = readLine().toDouble()
 //        var num1Double = num1.toDouble()
-        println("You entered $num1. Great! Now enter the second number.")
+        println("You entered ₱ $num1. Great! Now enter the second number.")
         println(enterHere)
 
        var num2 = readLine()!!.toDouble()
 //        var num2 = readLine().toDouble()
 //        var num2Double = num2.toDouble()
-        println("You entered $num2. Great! Now enter the third number.")
+        println("You entered ₱ $num2. Great! Now enter the third number.")
         println(enterHere)
 
         var num3 = readLine()!!.toDouble()
@@ -34,13 +34,13 @@ fun main() {
         var num4 : Double = readLine()!!.toDouble()
 //        var num4 : Double = readLine().toDouble()
 //        var num4Double = num4.toDouble()
-        println("You entered $num4. Great! Now enter the fifth number.")
+        println("You entered ₱ $num4. Great! Now enter the fifth number.")
         println(enterHere)
 
         var num5 : Double = readLine()!!.toDouble()
 //        var num5 : Double = readLine()!!.toDouble()
 //        var num5Double = num5.toDouble()
-        println("You entered $num5. Okay! Now enter a number to divide all numbers to.")
+        println("You entered ₱ $num5. Okay! Now enter a number to divide all numbers to.")
         println(enterHere)
 
         var divisor : Int = readLine()!!.toInt()
@@ -49,7 +49,7 @@ fun main() {
 
         println("Okay the answer is...")
 //        println(((num1Double + num2Double + num3Double +num4Double + num5Double)/divisorDouble).equals(Int))
-        println((num1+num2+num3+num4+num5).toInt()/divisor.toInt())
+        println("₱ ${(num1+num2+num3+num4+num5).toInt()/divisor.toInt()}")
 
     } else if (ready.uppercase() == "N") {
         println("Okay till next time!")
