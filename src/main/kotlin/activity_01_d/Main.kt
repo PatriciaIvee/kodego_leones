429201d9eacc31076359e18c803327ae1676befc
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-10-17 17:20:38 +0800

    This is my solution as of 10-17-2022 for Activity 01 D. I did while loops with if statements inside. Seems fine for now 0_0.

--- src/main/kotlin/activity_01_d/Main.kt
+++ src/main/kotlin/activity_01_d/Main.kt
@@ -1,7 +1,6 @@
 package activity_01_d
 
 import mu.KotlinLogging
-import java.util.*
 private val logger = KotlinLogging.logger{}
 fun main() {
     /**
@@ -12,89 +11,62 @@ fun main() {
      * Note: Error checking must be done.
      */
 
-
-//    SOLUTION 1 : READLINE
-    val applicationName : String = "Welcome to Exercise activity_01_d "
-
-    val enterHere : String = "Enter number : "
-    var ready = readLine()
-    logger.info ("Okay let's proceed enter the first number.")
-    logger.info { enterHere }
-
-
-    var num1 = readLine()!!.toDouble()
-    /*
-    var num1 = readLine()
-    var num1Double = num1.toDouble()
-    */
-    logger.info { "You entered ₱ $num1. Great! Now enter the second number." }
-    println(enterHere)
-
-    var num2 = readLine()!!.toDouble()
-    /*
-    var num2 = readLine()
-    var num2Double = num2.toDouble()
-    */
-    logger.info { "You entered ₱ $num2. Great! Now enter the third number." }
-    logger.info { enterHere }
-
-    var num3 = readLine()!!.toDouble()
-    /*
-    var num3 = readLine().toDouble()
-    var num3Double = num3.toDouble()
-    */
-    logger.info { "You entered $num3. Great! Now enter the fourth number." }
-    logger.info { enterHere }
-
-    var num4 : Double = readLine()!!.toDouble()
-    /*
-    var num4 = readLine()
-    var num4Double = num4.toDouble()
-    */
-    logger.info { "You entered ₱ $num4. Great! Now enter the fifth number." }
-    println(enterHere)
-
-    var num5 : Double = readLine()!!.toDouble()
-    /*
-    var num5 = readLine()
-    var num5Double = num5.toDouble()
-    */
-    logger.info { "You entered ₱ $num5. Okay! Now enter a number to divide all numbers to." }
-    logger.info { enterHere }
-
-    var lastInput : Int = readLine()!!.toInt()
-    /*
-    var divisor = readLine()
-    var divisorDouble = divisor.toDouble()
-    */
-
-    logger.info { "Okay the answer is..." }
-//        println(((num1Double + num2Double + num3Double +num4Double + num5Double)/divisorDouble).equals(Int))
-    logger.info {"₱ ${(num1+num2+num3+num4+num5).toInt()/lastInput.toInt()}" }
-
-
-//    SOLUTION 2 WHILE LOOP
-
-    logger.info("Input the 5 numbers : ")
-    var increment = 0
-    var number = 0
+//    SOLUTION 1
+    logger.info { "Welcome to Activity 01 D" }
+    logger.info { "Please enter 5 integers" }
+    var i = 1
     var sum = 0
 
-    while (increment < 5) {
-        val scanner = Scanner(System.`in`)
-        number = scanner.nextInt()
-        sum += number
-        increment++
-    }
-    logger.info{"The sum of 5 numbers is : $sum."}
-    logger.info("Enter one more number to divide to sum: ")
-    val scanner = Scanner(System.`in`)
-    var divisor = scanner.nextInt()
-    var total = sum / divisor
-    logger.info("The answer is $total")
+    while (i <= 5) {
+// while loop to enter 5 numbers
+        logger.info {"Input # $i"}
+        var userInput = readLine()
+// readLine = string value
+        if (userInput != null && userInput.all { char -> char.isDigit() } ) {
+            var userInputInt = userInput.toIntOrNull()
+//            it will convert string (readLine) into integer
+            if (userInputInt != null) {
+//                    it will check if the input is not blank
+                sum += userInputInt
+//                    user input integer is added until we reach 5 integer userInput
+            } else {
+//                    it will print this statement if the input is blank
+                logger.info { "User did not enter a value counted as null." }
+            }
+        } else {
+            logger.info { "Please enter an integer value : " }
+            i -= 1
+            /* it will deduct i if you didn't enter an integer.
+            It will continue the loop unless it reaches five integer userInputs
 
-    if (total is Int) {
-        logger.info { "$total is an integer" }
+             */
+        }
+//        if the input is an integer there is increment
+        ++i
+    }
+    logger.info { "The sum is : $sum" }
+    logger.info { "Please enter one more value as divisor: " }
+
+    while (i <= 6 ) {
+//since we stopped at 5 i we also created a loop for a correct divisor input (integer)
+        var userInputDivisor = readLine()
+        logger.info { "Input divisor $userInputDivisor" }
+// we create a new read line for divisor input
+        if (userInputDivisor != null && userInputDivisor.all { char -> char.isDigit() } ) {
+            var userInputInt = userInputDivisor.toIntOrNull()
+            if (userInputInt != null) {
+                var total = sum / userInputInt
+                logger.info { "The total is : $total" }
+            } else {
+                logger.info { "User did not enter a value counted as null" }
+                logger.info { "Sum : $sum" }
+                logger.info { sum / 0 } // program throws an error because anything divided by zero is invalid
+            }
+        }   else {
+            logger.info { "Please enter an integer value :" }
+            i -= 1
+        }
+        ++i
     }
 
 }
