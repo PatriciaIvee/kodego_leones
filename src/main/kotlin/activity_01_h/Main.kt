b79e40adbd4fd8efb07117792f90660cd3b848be
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-10-21 16:58:11 +0800

    Added loop Solution to activity_01_H Main.kt logger.info for Unique Keys

--- src/main/kotlin/activity_01_h/Main.kt
+++ src/main/kotlin/activity_01_h/Main.kt
@@ -13,20 +13,32 @@ fun main() {
      */
 
 //    SOLUTION
-
+    logger.info { "Welcome to Activity 01 H" }
     logger.info { "Please enter 2 integers" }
-    logger.info { "Start" }
+    logger.info { "Start of Range : " }
     var inputInt1 = readln().toInt()
-    logger.info { "End" }
-    var inputInt2 = readln().toInt()
-    var integerRange = inputInt1 .. inputInt2
+    logger.info { "End of Range :" }
+    val inputInt2 = readln().toInt()
+//    var integerRange = inputInt1 .. inputInt2
+    logger.info { "Prime numbers for range $inputInt1 to $inputInt2 .." }
+    while (inputInt1 < inputInt2) {
+//        loop to keep moving from start of range until the end.
+        var notPrimeNumber = false
+//        condition if is not a prime number
+        for (i in 2.. inputInt1 / 2) {
+//            number will move 2 places start range then /2 ?
+            if (inputInt1 % i == 0) {
+//                if the input1 Start is equal to 0 when divided by 2 no  remainder
+                notPrimeNumber = true
+                break
 
-    val list: MutableList<Long> = ArrayList()
-//    var index = 0
-//    do {
-//
-//
-//        index ++
-//    } while (index < 2)
+            }
+        }
+        if (!notPrimeNumber) {
+        logger.info ("Prime number : $inputInt1")
+//            will print the number if it is a prime number
+        }
+        ++ inputInt1
+    }
 
 }
