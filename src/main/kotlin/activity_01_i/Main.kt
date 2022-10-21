d7f6f5cd971f8df3e30aaf2d6816a0e9633429ca
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-10-21 18:53:01 +0800

    added partial answers for activity 01 i Main.kt

--- src/main/kotlin/activity_01_i/Main.kt
+++ src/main/kotlin/activity_01_i/Main.kt
@@ -1,5 +1,7 @@
 package activity_01_i
 
+import java.util.Date
+
 fun main() {
 
     /**
@@ -8,4 +10,14 @@ fun main() {
      * Goal: Keep track book borrowed, who borrowed and how long it can be borrowed.
      * Scope : Data Structures
      */
+
+    var userNameHashSet:Set<String> = HashSet<String>()
+    var logInDetail:Map<String, String> = mutableMapOf()
+    var bookNames:List<String> = mutableListOf<String>()
+    var isbnNumberList = ArrayList<String>()
+    var bookLocationMap:Map<String, Int> = mutableMapOf() //Section and bookshelf number
+    var timeBorrowedArrayList = ArrayList<Date>()
+    var timeReturnedArrayList = ArrayList<Date>()
+
+
 }
