2539dbbf2ec63a6c0afc042fc4d8264bdc2f97ad
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-10-21 15:33:07 +0800

    Added loop Solution to activity_01_g Main.kt

--- src/main/kotlin/activity_01_g/Main.kt
+++ src/main/kotlin/activity_01_g/Main.kt
@@ -14,17 +14,17 @@ fun main() {
      * GIRAFARIG - Palindrome
      * 101 - Palindrome
      * RACECAR - Palindrome
-     *  GATE - Not Palindrome
+     * GATE - Not Palindrome
      */
 
-//    SOLUTION
+//    SOLUTION : REVERSED METHOD FUNCTION
 
     val title = "Welcome to Activity 01 G"
     logger.info { title }
     logger.info { "Please Enter a String." }
 
     val inputString = readln()
-    var reverseInput = inputString.reversed()
+    val reverseInput = inputString.reversed()
 
     if (inputString.equals(reverseInput,ignoreCase = true)) {
         logger.info { "$inputString - Palindrome" }
@@ -32,4 +32,22 @@ fun main() {
         logger.info { "$inputString - Not Palindrome." }
     }
 
+// SOLUTION 2 : LOOP SOLUTION
+    logger.info { title }
+    logger.info { "Please Enter a String." }
+    val enterInput = readLine()
+
+    var reverseString = ""
+    if (enterInput != null) {
+        for (a in enterInput.length - 1 downTo 0) {
+            reverseString += enterInput[a]
+        }
+    }
+
+    logger.info { "Reversed String : $reverseString" }
+    if (reverseString == enterInput){
+        logger.info { "$enterInput - Palindrome" }
+    } else {
+        logger.info { "$enterInput - Not Palindrome" }
+    }
 }
