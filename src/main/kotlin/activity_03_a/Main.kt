104bab52fd9b537344a71937fe52c0844848b43a
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-11-05 22:25:49 +0800

    added activity_03_a, activity_03_b, and activity_03_c instructions and partial answers.

---
+++ src/main/kotlin/activity_03_a/Main.kt
@@ -0,0 +1,100 @@
+package activity_03_a
+
+
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger{}
+/**
+ * Covered Topic(s) : Functions
+ * Instructions :
+ * Note: Names are single word with no spaces example, "Marco", "Eugene"
+ * "Sarah", "Isaiah", "Anthony", "Henry".
+ *
+ * 1. Create an ArrayList of Names with 20 entries.
+ *
+ * 2. Create a function "isStudentInRecord" that will accept a name and
+ * return true if the name is found, otherwise false.
+ *
+ * 3. Create a function "addStudent" that will accept a name and ass it to the
+ * ArrayList.
+ *
+ * 4. Create a function "removeStudent" that will accept ta name and remove
+ * it from the ArrayList.
+ *
+ * 5. Create a function "countStudent" that will return the size of the ArrayList.
+ *
+ * 6. Create a function "searchStudentWildSearch" that will accept a String
+ *  and search if that string is found with in the ArrayList, it will return an
+ * ArrayList of names that matched if there are.
+ *
+ * 7. Create a function "searchStudentName" that will accept a String and
+ * search if there is an exact match of the String input, it will return an ArrayList of names
+ * that matched if there are.
+ *
+ * 8. Create a function "searchStudent" that will accept a String and call "searchStudentWildSearch" if
+ * the String input is less than or equal to three, and it will call "searchStudentName"
+ * if the String is greater than three.
+ *
+ * 9.Create a function "showStudents" that will print all the entries in the ArrayList.
+ *
+ */
+
+fun main(args: Array<String>){
+
+    val names :ArrayList<String> = ArrayList()
+    names.add("name1")
+    names.add("name2")
+    names.add("name3")
+    names.add("name4")
+    names.add("name5")
+    names.add("name6")
+    names.add("name7")
+    names.add("name8")
+    names.add("name9")
+    names.add("name10")
+    names.add("name11")
+    names.add("name12")
+    names.add("name13")
+    names.add("name14")
+    names.add("name15")
+    names.add("name16")
+    names.add("name17")
+    names.add("name18")
+    names.add("name19")
+    names.add("name20")
+    logger.info { names }
+
+}
+
+
+//    isStudentInRecord
+fun isStudentInRecord(name:String = String(),studentArrayList: ArrayList<String> = ArrayList()): Boolean {
+    if(studentArrayList.contains(name)){
+        logger.info { "Name $name is in student record : true" }
+        return true
+    }
+    logger.info {  "Name $name is in student record : false" }
+    return false
+}
+
+//    addStudent
+fun addStudent(name: String = String(), studentArrayList:ArrayList<String> = ArrayList<String>()) {
+    studentArrayList.add(name.toString())
+    logger.info { studentArrayList }
+    }
+
+//    removeStudent
+fun removeStudent(name: String = String(), studentArrayList:ArrayList<String> = ArrayList<String>()){
+    studentArrayList.remove(name)
+  }
+
+//    countStudent
+fun countStudent(studentArrayList:ArrayList<String> = ArrayList<String>()){
+    logger.info { studentArrayList.size }
+}
+
+fun searchStudentWildSearch(){}
+fun searchStudentName(){}
+fun searchStudent(){}
+fun showStudents(){}
---
+++ src/main/kotlin/activity_03_b/Main.kt
@@ -0,0 +1,63 @@
+package activity_03_b
+
+/**
+ * Covered Topic(s) : Functions
+ * Instructions :
+ * 1. Create an ArrayList of bookname with 20 entries.
+ * 2. Create a function "isBookInRecord" that will accept a String
+ * and return true if the book is found, otherwise false.
+ * 3. Create a function "addBook" that will accept a String and add it to the ArrayList.
+ * 4. Create a function "removeBook"  that will accept a name and remove it from the
+ * ArrayList if it exactly matches the book name.
+ * 5. Create a function "countBooks" that will return the size of the ArrayList.
+ * 6. Create a function "searchBookWildSearch" that will accept a String and search if
+ * that string is found with in the ArrayList,
+ * it will return an ArrayList of books that matched if there are.
+ * 7. Create a function  "searchBookName" that will accept a String and search if
+ * there is an exact match of the String input,
+ * it will return an ArrayList of books that matched if there are.
+ * 8.  Create a function "showBooks" that will print all the entries in the ArrayList.
+ */
+fun main() {
+    var bookName:ArrayList<String> = ArrayList()
+    bookName.add("Book1")
+    bookName.add("Book2")
+    bookName.add("Book3")
+    bookName.add("Book4")
+    bookName.add("Book5")
+    bookName.add("Book6")
+    bookName.add("Book7")
+    bookName.add("Book8")
+    bookName.add("Book9")
+    bookName.add("Book10")
+    bookName.add("Book11")
+    bookName.add("Book12")
+    bookName.add("Book13")
+    bookName.add("Book14")
+    bookName.add("Book15")
+    bookName.add("Book16")
+    bookName.add("Book17")
+    bookName.add("Book18")
+    bookName.add("Book19")
+    bookName.add("Book20")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
