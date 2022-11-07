f04defe73254701dd79876143de7f4fb9bba3fe8
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-11-07 09:28:14 +0800

    added answer for activity_03_b Main.kt

--- src/main/kotlin/activity_03_b/Main.kt
+++ src/main/kotlin/activity_03_b/Main.kt
@@ -1,5 +1,8 @@
 package activity_03_b
 
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger{}
 /**
  * Covered Topic(s) : Functions
  * Instructions :
@@ -19,29 +22,71 @@ package activity_03_b
  * 8.  Create a function "showBooks" that will print all the entries in the ArrayList.
  */
 fun main() {
-    var bookName:ArrayList<String> = ArrayList()
-    bookName.add("Book1")
-    bookName.add("Book2")
-    bookName.add("Book3")
-    bookName.add("Book4")
-    bookName.add("Book5")
-    bookName.add("Book6")
-    bookName.add("Book7")
-    bookName.add("Book8")
-    bookName.add("Book9")
-    bookName.add("Book10")
-    bookName.add("Book11")
-    bookName.add("Book12")
-    bookName.add("Book13")
-    bookName.add("Book14")
-    bookName.add("Book15")
-    bookName.add("Book16")
-    bookName.add("Book17")
-    bookName.add("Book18")
-    bookName.add("Book19")
-    bookName.add("Book20")
+    var bookNames:ArrayList<String> = ArrayList()
+    bookNames.add("Book1")
+    bookNames.add("Book2")
+    bookNames.add("Book3")
+    bookNames.add("Book4")
+    bookNames.add("Book5")
+    bookNames.add("Book6")
+    bookNames.add("Book7")
+    bookNames.add("Book8")
+    bookNames.add("Book9")
+    bookNames.add("Book10")
+    bookNames.add("Book11")
+    bookNames.add("Book12")
+    bookNames.add("Book13")
+    bookNames.add("Book14")
+    bookNames.add("Book15")
+    bookNames.add("Book16")
+    bookNames.add("Book17")
+    bookNames.add("Book18")
+    bookNames.add("Book19")
+    bookNames.add("Book20")
+
+    logger.info { "Check if book is in the record: " }
+    isBookInRecord(bookArrayList = bookNames)
 }
 
+//isBookInRecord
+fun isBookInRecord(bookName:String = readln(),bookArrayList: ArrayList<String> = ArrayList()){
+    logger.info { "Is book in record?:" }
+    if (bookArrayList.contains(bookName)){
+        logger.info{ true }
+    } else{
+        logger.info { false }
+    }
+
+}
+
+//addBook
+fun addBook(bookName:String = readln(),bookArrayList: ArrayList<String> = ArrayList()){
+    bookArrayList.add(bookName)
+}
+//removeBook
+fun removeBook(bookName:String = readln(),bookArrayList: ArrayList<String> = ArrayList()){
+    bookArrayList.add(bookName)
+}
+//countBooks
+fun countBooks(bookName:String = readln(),bookArrayList: ArrayList<String> = ArrayList()){
+    logger.info{"Number of books in the list: ${ bookArrayList.size }"}
+}
+//searchBookWildSearch
+fun searchBookWildSearch(bookName:String = readln(),bookArrayList: ArrayList<String> = ArrayList()){
+    val wildSearch = bookArrayList.filter {
+        it.contains(bookName, ignoreCase = true)
+    }
+    wildSearch.forEach { bookNames -> println(bookNames) }
+}
+//searchBookName
+fun searchBookName(bookName:String = readln(),bookArrayList: ArrayList<String> = ArrayList()){
+    val exactSearch = bookArrayList.filter { it == bookName }
+    exactSearch.forEach { bookNames -> println(bookNames) }
+}
+//showBooks
+fun showBooks(bookArrayList: ArrayList<String> = ArrayList()){
+    bookArrayList.forEach { bookNames -> println(bookNames) }
+}
 
 
 
