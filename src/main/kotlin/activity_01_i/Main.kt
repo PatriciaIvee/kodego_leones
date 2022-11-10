e4300e1d893a2b4ad10a502290c675195020034b
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-11-10 17:53:03 +0800

    Edited answer for activity 01 i Main.kt

--- src/main/kotlin/activity_01_i/Main.kt
+++ src/main/kotlin/activity_01_i/Main.kt
@@ -10,56 +10,15 @@ fun main() {
      * Goal: Keep track book borrowed, who borrowed and how long it can be borrowed.
      * Scope : Data Structures
      */
+/*
+Process:
+1. Enter Username and password
+2. Welcome UserName
+3. Borrow a book? Yes or no Loop
+4. Check books borrowed
+5. Display books borrowed and burrow limit days
+ */
 
-//   Login, password username and details
-    var userNameHashSet:Set<String> = HashSet<String>()
-    var passwordArrayList = ArrayList<String>()
-    var logInDetail:Map<String, String> = mutableMapOf() //Name and Library Card
-
-//    Contact details
-    var lastNameArrayList = ArrayList<String>()
-    var firstNameArrayList = ArrayList<String>()
-    var middleInitialArrayList =ArrayList<Char>()
-    var suffixArrayList =  ArrayList<String?>()
-    var phoneNumberArrayList = ArrayList<Long>()
-    var addressArrayList = ArrayList<String>()
-    var landlineNumberArrayList =ArrayList<Int?>()
-
-//    Book information location and time borrowed
-    var eBookTitlesArrayList = ArrayList<String>()
-    var bookTitlesArrayList = ArrayList<String>()
-    var authorsArrayList = ArrayList<String>()
-    var bookIdsArrayList = ArrayList<String>()
-    var isbnBookNumberArrayList = ArrayList<String>()
-    var eIsbnBookNumberArrayList = ArrayList<String>()
-    var publisherArrayList = ArrayList<String?>()
-    var publishedDateArrayList = ArrayList<Date>()
-    var editionArrayList = ArrayList<String?>()
-    var numberOfPagesArrayList = ArrayList<Int?>()
-    var numberOfCopiesArrayList = ArrayList<Int?>()
-    var shelfNumberArrayList = ArrayList<Int?>()
-    var copyrightArrayList = ArrayList<String>()
-    var subjectArrayList = ArrayList<String?>()
-    var classificationArrayList = ArrayList<String>()
-    var languagesArrayList = ArrayList<String>()
-    var eBookFileSizeArrayList = ArrayList<Int?>()
-    var totalBorrowedBooks = ArrayList<Int?>()
-
-    var bookLocationMap:Map<String, Int> = mutableMapOf() //Section and bookshelf number
-    var timeBorrowedArrayList = ArrayList<Date>()
-    var timeReturnedArrayList = ArrayList<Date>()
-    var bookBorrowerArrayList = ArrayList<String>()
-    var dayLimitBorrowSet:Set<CharRange> = mutableSetOf<CharRange>()
-
-//  Information about items borrowed in a library that are not books
-    var musicResourcesArrayList = ArrayList<String>()
-    var itemIdNumberArrayList = ArrayList<String>()
-    var electronicEquipmentResourcesArrayList = ArrayList<String>()
-    var newsArticleResourcesArrayList = ArrayList<String>()
-    var mapResourcesArrayList = ArrayList<String>()
-    var visualMaterialResourceArrayList = ArrayList<String>()
-    var typeArrayList = ArrayList<String>()
-    var itemsTotalArrayList = ArrayList<Int>()
-
+//    Code it
 
 }
