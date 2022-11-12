5de2ace3b48362b6d402314ae42b81a4df0751bc
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-11-12 19:55:56 +0800

    added partial solution activity 04 c Main.kt

--- src/main/kotlin/activity_04_c/Main.kt
+++ src/main/kotlin/activity_04_c/Main.kt
@@ -20,3 +20,75 @@ package activity_04_c
  * Add 5 more type of items you can find in the grocery.
  * Identify the attributes for all the classes mentioned.
  */
+
+class Poultry {
+
+}
+
+class Fish {
+
+}
+class CannedGoods{
+
+}
+class Snacks {
+
+}
+
+class FrozenProducts {
+
+}
+
+class Fruits{
+
+}
+class Vegetables{
+
+}
+
+class Condiments{
+
+}
+
+class Furniture{
+
+}
+
+class Appliances{
+
+}
+class Clothes{
+
+}
+
+class Pet{
+
+}
+
+class SanitaryProducts{
+
+}
+
+class PersonalCare {
+
+}
+
+//Five Additional type of items I find in grocery store
+class Beverages {
+
+}
+class WinesAndSpirits {
+
+}
+class ImportedGoods{
+
+}
+
+class Stationary {
+
+}
+
+class BreadAndPastries{
+
+}
+
