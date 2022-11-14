27f9b6d60bc9a425c6c194d8bb6a538b084eb1c6
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-11-14 20:27:49 +0800

    added solution to exercise_object_oriented_programming Main.kt

--- src/main/kotlin/exercise_object_oriented_programming/Main.kt
+++ src/main/kotlin/exercise_object_oriented_programming/Main.kt
@@ -18,31 +18,82 @@ package exercise_object_oriented_programming
  * - Use OOP concepts to create the classes for the food category.
  * - You must use Inheritance, Polymorphism, Encapsulation, Abstraction
  */
+//need to implement polymorphism
+//abstraction is get and set
+open class Product {
+    var name:String = ""
+        private set //var name cannot be changed
+    var price: Double = 0.0
 
-open class Cart {
-    var customer: ArrayList<Customer> = ArrayList()
-    var orderNumber: Set<Int> = mutableSetOf()
-    var orders:Map<String,Int> = HashMap()
-    var ordersPrice:Map<String,Double> = HashMap()
-
-    class Customer{
-        var lastName: String = ""
-        var firstName: String = ""
-        var mobileNumber: String = ""
-        var address: String = ""
+    constructor(name: String, price: Double){
+        this.name = name
+        this.price = price
     }
 }
 
-enum class Status {
-    PREPARING,
+
+class Fruits(name : String, price: Double):Product(name, price) {
+    var quantity: Float = 0.0F
+    var quantityUnits: String = "per Piece"
+
+}
+class Shake(name : String, price: Double):Product(name, price) {
+    var size: Any  = ""
+    var ingredients : ArrayList<String> = ArrayList()
+    var addons : ArrayList<Any> = ArrayList()
+
+}
+
+class Juices(name : String, price: Double):Product(name, price) {
+
+    var size: Any  = ""
+    var ingredients : ArrayList<String> = ArrayList()
+}
+
+data class Sandwiches(val name: String) {
+    var price: Double = 0.0
+    var ingredients : ArrayList<String> = ArrayList()
+    var addons : ArrayList<Any> = ArrayList()
+    var breadType: Any = ""
+}
+
+class Salad(name : String, price: Double):Product(name, price) {
+    var size: Any  = ""
+    var ingredients : ArrayList<String> = ArrayList()
+    var dressing: Any = ""
+    var addons : ArrayList<Any> = ArrayList()
+}
+
+
+class Cart {
+    var uniqueID:String = ""
+    lateinit var customer:Customer
+    var items:HashMap<Product,Float> = HashMap() // polymorphism
+    var status:OrderStatus = OrderStatus.UNKNOWN
+
+    fun updateOrder(status: OrderStatus){
+        this.status = status
+    }
+
+    fun addItems(product: Product, quantity: Float){
+//        items.put(product,quantity)
+        items[product] = quantity
+    }
+
+    data class Customer(var address: String,
+                        var mobileNumber: String,
+                        var firstName: String,
+                        var lastName:String){
+    }
+}
+
+enum class OrderStatus {
+    SENT_TO_KITCHEN,
+    BEING_PREPARED,
     FOR_PICKUP,
     FOR_DELIVERY,
     DELIVERED,
+    PICKED_UP,
     CANCELLED,
-    PAID,
     UNKNOWN
-}
-
-fun updateOrder(){
-
 }
