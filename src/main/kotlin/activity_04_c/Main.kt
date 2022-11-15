f2e47a0dda04b00430118efcdf6247099535b8a1
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-11-15 19:53:12 +0800

    added solution to activity 04 c Main.kt

--- src/main/kotlin/activity_04_c/Main.kt
+++ src/main/kotlin/activity_04_c/Main.kt
@@ -1,5 +1,7 @@
 package activity_04_c
 
+import java.util.*
+
 /**
  *Covered Topic(s) : Classes
  * Create class for
@@ -21,74 +23,132 @@ package activity_04_c
  * Identify the attributes for all the classes mentioned.
  */
 
-class Poultry {
+class Poultry(name: String, price: Double) {
+    val weight: Float = 0.0F
+}
+
+class Fish(name: String, price: Double) {
+    var weight: Float = 0.0F
+}
+class CannedGoods(name: String, price: Double) {
+    var weight: Float = 0.0F
+    var manufactureDate = Date()
+    var batch: String = ""
+    var expirationDate = Date()
+    var pieces:Int = 0
+}
+class Snacks(name: String, price: Double) {
+    var weight: Float = 0.0F
+    var manufactureDate = Date()
+    var batch: String = ""
+    var expirationDate = Date()
+    var pieces:Int = 0
+}
+
+class FrozenProducts(name: String, price: Double) {
+    var weight: Float = 0.0F
+}
+
+class Fruits(name: String, price: Double) {
+    var weight: Float = 0.0F
+}
+class Vegetables(name: String, price: Double) {
+    var weight: Float = 0.0F
+}
+
+class Condiments(name: String, price: Double) {
+    var weight: Float = 0.0F
+    var manufactureDate = Date()
+    var batch: String = ""
+    var expirationDate = Date()
+}
+
+class Furniture(name: String, price: Double){
+    var width: Float = 0.0F
+    var length: Float = 0.0F
+    var height: Float = 0.0F
+    var material: String = ""
+    var color: String = ""
+}
+
+class Appliances(name: String, price: Double){
+    var width: Float = 0.0F
+    var length: Float = 0.0F
+    var height: Float = 0.0F
+    var finish: String = ""
+    var color: String = ""
+    var manufactureDate =Date()
+    var batch: String = ""
+
+}
+class Clothes(name: String, price: Double){
+    var size: String = ""
+    var width: Float = 0.0F
+    var length: Float = 0.0F
+    var material: String = ""
+    var color: String = ""
+}
+
+class Pet(name: String, price: Double){
+    var weight: Float = 0.0F
+    var manufactureDate = Date()
+    var batch: String = ""
+    var expirationDate = Date()
+    var pieces:Int = 0
 
 }
 
-class Fish {
-
-}
-class CannedGoods{
-
-}
-class Snacks {
-
+class SanitaryProducts(name: String, price: Double){
+    var weight: Float = 0.0F
+    var manufactureDate = Date()
+    var batch: String = ""
+    var pieces:Int = 0
 }
 
-class FrozenProducts {
-
-}
-
-class Fruits{
-
-}
-class Vegetables{
-
-}
-
-class Condiments{
-
-}
-
-class Furniture{
-
-}
-
-class Appliances{
-
-}
-class Clothes{
-
-}
-
-class Pet{
-
-}
-
-class SanitaryProducts{
-
-}
-
-class PersonalCare {
+class PersonalCare(name: String, price: Double) {
+    var weight: Float = 0.0F
+    var manufactureDate = Date()
+    var batch: String = ""
+    var expirationDate = Date()
+    var pieces:Int = 0
 
 }
 
 //Five Additional type of items I find in grocery store
-class Beverages {
-
+class Beverages(name: String, price: Double) {
+    var weight: Float = 0.0F
+    var manufactureDate = Date()
+    var batch: String = ""
+    var expirationDate = Date()
 }
-class WinesAndSpirits {
-
+class WinesAndSpirits(name: String, price: Double) {
+    var weight: Float = 0.0F
+    var manufactureDate = Date()
+    var batch:String = ""
+    var expirationDate= Date()
 }
-class ImportedGoods{
-
+class ImportedGoods(name: String, price: Double){
+    var weight: Float = 0.0F
+    var manufactureDate = Date()
+    var batch:String = ""
+    var expirationDate= Date()
+    var pieces: Int = 0
 }
 
-class Stationary {
-
+class Stationary(name: String, price: Double) {
+    var weight: Float = 0.0F
+    var batch:String = ""
+    var material: String= ""
+    var pieces: Int = 0
 }
 
-class BreadAndPastries{
-
+class BreadAndPastries(name: String, price: Double) {
+    var weight: Float = 0.0F
+    var manufactureDate = Date()
+    var batch:String = ""
+    var expirationDate= Date()
+    var pieces: Int = 0
 }
+
+
 
