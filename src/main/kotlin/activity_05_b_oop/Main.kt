eb6e3ada714922744cb49966a0205ce0e48e87ae
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-11-19 13:27:53 +0800

    added instruction for activity 05 a  and b Main.kt

---
+++ src/main/kotlin/activity_05_b_oop/Main.kt
@@ -0,0 +1,14 @@
+package activity_05_b_oop
+
+/**
+ * 1. A book, a magazine, a newspaper and comics inherits from a Publication class.
+ * Identify the attributes and the methods for the publication.
+ *
+ * 2. There are several Audio / Video materials, Recordings, Documentary, Movies and powerpoint materials.
+ * What will you use to present the different types of Audio / Video presentation, a class or an enum? Why?
+ * Implement your answer in code.
+ *
+ * 3. Authors and Illustrators seem to have similar data?
+ * What will you use to present the two a class or an enum or will you keep them as is? Why?
+ * Implement your answer in code.
+ */
---
+++ src/main/kotlin/activity_05_c_oop/Main.kt
@@ -0,0 +1,12 @@
+package activity_05_c_oop
+
+/**
+ * Covered Topic(s) : OOP
+ * 1. Create a parent class for all the products in the grocery.
+ * 2. Create a cart to store the groceries.
+ * 3. Create a Main class where you will add the Cart.
+ * 4. In the main class add the following functions
+ * a - add to cart > add an item in the cart
+ * b - remove from cart > remove item in the cart
+ * c - check out cart > compute the total cost of the cart.
+ */
