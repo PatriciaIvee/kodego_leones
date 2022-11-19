bd1ffde42f2b3c4699b8001ff3fb7560f4218414
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-11-19 14:21:27 +0800

    added partial answer for activity 05 b Main.kt

--- src/main/kotlin/activity_05_b_oop/Main.kt
+++ src/main/kotlin/activity_05_b_oop/Main.kt
@@ -1,5 +1,8 @@
 package activity_05_b_oop
 
+import activity_04_b.Article
+import java.util.Date
+
 /**
  * 1. A book, a magazine, a newspaper and comics inherits from a Publication class.
  * Identify the attributes and the methods for the publication.
@@ -12,3 +15,49 @@ package activity_05_b_oop
  * What will you use to present the two a class or an enum or will you keep them as is? Why?
  * Implement your answer in code.
  */
+
+open class Person(var firstName: String, var lastName: String) {
+    var middleName:String = ""
+    var birthDate:Date = Date()
+
+    fun fullName() = "$lastName, $firstName $middleName"
+
+}
+
+
+open class Publication{
+    var title: String = ""
+    var datePublished: Date = Date()
+    var edition:String = ""
+    var ISBN: String = ""
+
+}
+
+class Book:Publication(){
+
+    var authors: ArrayList<Authors> = ArrayList()
+    var illustrators:ArrayList<Illustrator> = ArrayList()
+    var chapters:ArrayList<String> = ArrayList()
+    var pages: Int = 0
+
+    class Authors(firstName: String,lastName: String): Person(firstName, lastName)
+    class Illustrator(firstName: String, lastName: String):Person(firstName,lastName)
+}
+class Magazine: Publication(){
+    var articles: ArrayList<Article> = ArrayList()
+
+    class Illustrator(firstName: String, lastName: String):Person(firstName,lastName) {
+    }
+}
+class Newspaper: Publication(){
+    var headline: String = ""
+    var articles: ArrayList<Article> = ArrayList()
+    class Illustrator(firstName: String, lastName: String) :Person(firstName,lastName) {
+    }
+
+
+}
+
+class Comics: Publication(){
+
+}
