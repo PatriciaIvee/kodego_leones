067034fee1f99b6ef81fcca56972eb00f9f51a56
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-11-23 19:46:47 +0800

    added partial answer for activity 05 c oop

--- src/main/kotlin/activity_05_c_oop/Main.kt
+++ src/main/kotlin/activity_05_c_oop/Main.kt
@@ -10,3 +10,38 @@ package activity_05_c_oop
  * b - remove from cart > remove item in the cart
  * c - check out cart > compute the total cost of the cart.
  */
+
+fun main() {
+
+}
+open class Product(var productName: String,
+                   var productPrice: Double)
+
+class Poultry(productName: String,productPrice: Double): Product(productName, productPrice){}
+class Fish(productName: String,productPrice: Double): Product(productName, productPrice){}
+class CannedGoods(productName: String,productPrice: Double): Product(productName, productPrice){}
+class Snacks(productName: String,productPrice: Double): Product(productName, productPrice){}
+class FrozenGoods(productName: String,productPrice: Double): Product(productName, productPrice){}
+class Fruits(productName: String,productPrice: Double): Product(productName, productPrice){}
+class Vegetables(productName: String,productPrice: Double): Product(productName, productPrice){}
+class Condiments(productName: String,productPrice: Double): Product(productName, productPrice){}
+class Furniture(productName: String,productPrice: Double): Product(productName, productPrice){}
+class Appliances(productName: String,productPrice: Double): Product(productName, productPrice){}
+class Clothes(productName: String,productPrice: Double): Product(productName, productPrice){}
+class Pet(productName: String,productPrice: Double): Product(productName, productPrice){}
+class SanitaryAndCleaning(productName: String,productPrice: Double): Product(productName, productPrice){}
+class PersonalCare(productName: String,productPrice: Double): Product(productName, productPrice){}
+
+class Cart(cart:HashMap<String,Double>){}
+
+fun addToCart(){
+
+}
+
+fun removeToCart(){
+
+}
+
+fun checkOutCart(){
+
+}
