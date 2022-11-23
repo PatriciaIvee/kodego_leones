bb32a61affa5e27e0d79f3a7db0648712b481bec
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-11-23 20:14:15 +0800

    added activity 05 d oop with instructions

---
+++ src/main/kotlin/activity_05_d_oop/Main.kt
@@ -0,0 +1,15 @@
+package activity_05_d_oop
+
+/**
+ * Coverage : OOP Inheritance, Encapsulation, Polymorphism, Abstraction
+ *
+ * You are tasked to create a game.
+ * The game is snake and ladders.
+ * Identify and create the class that will be needed to implement Snakes and ladders.
+ *
+ * Hint:
+ *
+ * You will need an array to represent the board.
+ * You will need snakes, ladders and players.
+ * Use all OOP concepts.
+ */
