128c19bdd8d516b012b6ba38df0b8f81177fa297
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-11-26 20:41:06 +0800

    added notes for Abstraction.kt

--- src/main/kotlin/oop/Abstraction.kt
+++ src/main/kotlin/oop/Abstraction.kt
@@ -2,26 +2,40 @@ package oop
 
 
 interface PersonDAO{
-    var connectedToDatabase: Boolean
-        get() = false
-        set(value) {}
+//    var connectedToDatabase: Boolean
+//        get() = false
+//        set(value) {}
 //    need to override this to implement
 
     fun addPerson(person: Person)
     fun getPersons(): ArrayList<Person>
     fun searchPerson(firstName:String = "", middleName:String = "", lastName:String = "")
 
-
+/*
+ * interface is data access object
+ * it lists down all database access
+ * we only define what we can do in the database, but it doesn't have implementation
+ * we know how this works (interface) but we don't know how this works
+ * we know input and output of each method. We simply just need to copy and paste it
+ * in personStub and implement it
+ */
 }
 
 class PersonStub(): PersonDAO{
+
+    /*
+     * Stub is assigned, act as a placeholder for the database
+     * to replicate database operations defined in personDAO
+     * stub mimics how database works so when the actual database is
+     * now available we can easily change it to the actual database
+     */
     var personRecords: ArrayList<Person> = ArrayList()
 
-    override var connectedToDatabase: Boolean = false
-        get() = super.connectedToDatabase
-        set(value) {
-            field = value
-        }
+//    override var connectedToDatabase: Boolean = false
+//        get() = super.connectedToDatabase
+//        set(value) {
+//            field = value
+//        }
     override fun addPerson(person: Person) {
         personRecords.add(person)
     }
@@ -86,11 +100,22 @@ fun main() {
 //}
 
 abstract class Shape{
+
     protected var width = 0.0
     protected var height = 0.0
     protected var length = 0.0
     protected var sides = 0
     protected var dimensions = 0
+
+/*
+ * difference of abstract method and interface is:
+ *
+ * in interface, we just want it to contain all the required functions
+ * methods in the database(example)
+ *
+ * in abstract there is logic already also we already have implementation
+ *
+ */
 
     abstract fun getArea() :Double
     abstract fun getPerimeter():Double
@@ -99,40 +124,38 @@ abstract class Shape{
     fun fillColor(color:String){
 
     }
-
     fun resize(){
 
     }
-
     fun changeOutlineColor(color:String){
 
     }
 }
-//we cannot instansiate an interface
+//we cannot instantiate an interface
 
 class Rectangle: Shape(){
 //  We  Extend this
-    override fun getArea(): Double =length*width
+    override fun getArea(): Double =length * width
 
 
     override fun getPerimeter(): Double = 2 *(length * width)
 
 
-    override fun getVolume(): Double = length *width * height
+    override fun getVolume(): Double = length * width * height
 
 }
 
 class Triangle: Shape(){
     override fun getArea(): Double {
-        TODO("Not yet implemented")
+        TODO("Not yet implemented whatever the formula for Triangle is")
     }
 
     override fun getPerimeter(): Double {
-        TODO("Not yet implemented")
+        TODO("Not yet implemented whatever the formula for Triangle is")
     }
 
     override fun getVolume(): Double {
-        TODO("Not yet implemented")
+        TODO("Not yet implemented whatever the formula for Triangle is")
     }
 
 }
