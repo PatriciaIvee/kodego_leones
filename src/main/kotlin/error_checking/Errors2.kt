aee3217099e081cf776d454f2d90ed0b3e4a8b37
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-11-26 10:18:31 +0800

    added notes for ErrorChecking

--- src/main/kotlin/error_checking/Errors2.kt
+++ src/main/kotlin/error_checking/Errors2.kt
@@ -2,13 +2,20 @@ package error_checking
 
 fun main() {
     try {
+//        10/0
         lateinit var emptyClass: EmptyClass
         emptyClass = EmptyClass("Something")
         emptyClass.doSomething()
 
         var mobileNumber: String?  = null
+        println("${mobileNumber!!.length}")
 
+    }catch (ae:ArithmeticException){
+        println("You divided by zero")
+    }catch (ue:UninitializedPropertyAccessException){
+        println("Please instantiate the class you used")
     }catch (e:Exception){
+        println("Something went wrong")
         e.printStackTrace()
     }finally {
 
