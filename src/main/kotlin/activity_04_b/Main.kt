6ae7d5ed178fffc53cc8048184e2a9473354cb09
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-11-28 16:51:05 +0800

    done with activity 04 b for now

--- src/main/kotlin/activity_04_b/Main.kt
+++ src/main/kotlin/activity_04_b/Main.kt
@@ -1,7 +1,11 @@
 package activity_04_b
 
 
+import mu.KotlinLogging
+import java.sql.Time
 import java.util.Date
+
+private val logger = KotlinLogging.logger {  }
 
 /**
  * Covered Topic(s) : Classes
@@ -31,6 +35,11 @@ import java.util.Date
  * 10. Create a Publisher class.
  * A publisher a name, address and date established.
  */
+
+
+open class ReadingMaterial(){
+//    class that will be inherited sa book, Magazine ,etc.
+}
 
 class Book {
     var title:String = ""
@@ -96,11 +105,116 @@ class Publishers {
     var dateEstablished = Date()
 }
 
-class AudioVideoRecording {
-    var containsVideoRecording: Boolean = false
-    var containsAudioVideoRecording: Boolean = false
-    var containsBothVideoAudioVideoRecording: Boolean = false
+class Recording {
+    var title: String = ""
+    var publishers: ArrayList<Publishers> = ArrayList()
+    var length:Time = Time(0,0,0)
+    var datePublished: Date  = Date(1111,1,1)
+
+    var containsVideo: Boolean = false
+    var containsAudio: Boolean = false
+    var containsBothVideoAudio: Boolean = false
 }
 
-fun searchTitle(){}
-fun listThis(){}
+
+//Not yet sure how to combine wildSearch to all classes code doesn't allow to have multiple arraylist parameter
+fun wildSearchBook(name: String, readingMaterial: ArrayList<Book>,) {
+
+    val searchResults: ArrayList<String> = ArrayList()
+
+    if (name.length < 3){
+        logger.info { "String length should be 3 or more" }
+    }else{
+        for (book in readingMaterial){
+            if(book.title.contains(name,ignoreCase = true)){
+                searchResults.add(book.title)
+            }
+        }
+    }
+
+    println("Search Results:")
+    for(result in searchResults){
+       println(result)
+    }
+
+}
+
+fun wildSearchMagazine(name: String, readingMaterial: ArrayList<Magazine>,) {
+
+    val searchResults: ArrayList<String> = ArrayList()
+
+    if (name.length < 3){
+        logger.info { "String length should be 3 or more" }
+    }else{
+        for (magazine in readingMaterial){
+            if(magazine.title.contains(name,ignoreCase = true)){
+                searchResults.add(magazine.title)
+            }
+        }
+    }
+
+    println("Search Results:")
+    for(result in searchResults){
+        println(result)
+    }
+
+}
+
+fun wildSearchNewspaper(name: String, readingMaterial: ArrayList<Newspaper>) {
+
+    val searchResults: ArrayList<String> = ArrayList()
+
+    if (name.length < 3){
+        logger.info { "String length should be 3 or more" }
+    }else{
+        for (newspaper in readingMaterial){
+            if(newspaper.name.contains(name,ignoreCase = true)){
+                searchResults.add(newspaper.name)
+            }
+        }
+    }
+
+    println("Search Results:")
+    for(result in searchResults){
+        println(result)
+    }
+
+}
+
+fun wildSearchComics(name: String, readingMaterial: ArrayList<Comics>,) {
+
+    val searchResults: ArrayList<String> = ArrayList()
+
+    if (name.length < 3){
+        logger.info { "String length should be 3 or more" }
+    }else{
+        for (comics in readingMaterial){
+            if(comics.comicTitle.contains(name,ignoreCase = true)){
+                searchResults.add(comics.comicTitle)
+            }
+        }
+    }
+
+    println("Search Results:")
+    for(result in searchResults){
+        println(result)
+    }
+
+}
+
+fun listThisBook(addBook:Book,books: ArrayList<Book>){
+    books.add(addBook)
+
+}
+
+fun listThisMagazine(addMagazine: Magazine, magazines:ArrayList<Magazine>) {
+    magazines.add(addMagazine)
+}
+
+fun listThisNewspaper(addNewspaper: Newspaper, newspapers:ArrayList<Newspaper>){
+    newspapers.add(addNewspaper)
+}
+
+fun listThisComic(addComic:Comics,comics:ArrayList<Comics>){
+    comics.add(addComic)
+}
