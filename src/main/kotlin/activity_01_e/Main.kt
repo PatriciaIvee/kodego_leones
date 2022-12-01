4730127e74a00eb0825bd8570a23a4dd256b2305
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-12-01 19:22:38 +0800

    added activity 01 e

---
+++ src/main/kotlin/activity_01_e/Main.kt
@@ -0,0 +1,22 @@
+package activity_01_e
+
+
+fun main() {
+    val deckOfCards = arrayOf("Hearts","Diamonds", "Clubs", "Spades")
+    val airplaneSeatTicket: Array<String>  = arrayOf("001", "002", "003", "004")
+    val colorsOfRainbow : Array<String> = arrayOf("Red","Orange", "Yellow", "Green", "Blue", "Indigo", "Violet")
+
+    val notes:ArrayList<String> = ArrayList()
+    notes.add("Good Morning")
+
+    val weddingGuests:ArrayList<String> = ArrayList()
+    val enrolleesName: ArrayList<String> = ArrayList()
+
+    val productModel: HashSet<String> = HashSet()
+    val authenticityCard: Set<Int> = HashSet()
+    val usernames: Set<String> = HashSet()
+
+    val packageLocation : HashMap<String, String> = HashMap()
+    val password: HashMap<String, String> = HashMap()
+    val phoneDirectory: HashMap<String, String> = HashMap()
+}
