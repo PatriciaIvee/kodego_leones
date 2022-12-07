bb3ca2a16f9465074bfe38b82e7ddcc921a14b67
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-12-07 18:53:17 +0800

    added partial answer

--- src/main/kotlin/activity_05_d_oop/Main.kt
+++ src/main/kotlin/activity_05_d_oop/Main.kt
@@ -1,8 +1,7 @@
 package activity_05_d_oop
 
 import mu.KotlinLogging
-import notes.rollDice
-import java.util.*
+import kotlin.collections.ArrayList
 
 private val logger = KotlinLogging.logger {  }
 /**
@@ -29,8 +28,11 @@ private val logger = KotlinLogging.logger {  }
  * when you get to a ladder you go up
  *
  */
-val snakesAndLaddersBoard = mapOf( 4 to 14,  9 to 31, 17 to  7, 20 to 38, 28 to 84, 40 to 59, 51 to 67, 54 to 34,
-    62 to 19, 63 to 81, 64 to 60, 71 to 91, 87 to 24, 93 to 73, 95 to 75, 99 to 78)
+val snakesAndLaddersBoard = mapOf(
+    4 to 14, 9 to 31, 17 to 7, 20 to 38, 28 to 84,
+    40 to 59, 51 to 67, 54 to 34, 62 to 19, 63 to 81,
+    64 to 60, 71 to 91, 87 to 24, 93 to 73, 95 to 75,
+    99 to 78)
 
 interface MovePlayers{
     fun rollDice(): Int {
@@ -55,40 +57,65 @@ class RollAgain:MovePlayers {
     override fun rollDice(): Int {
         return super.rollDice()
     }
-
+}
+fun addPlayer(player: Player): ArrayList<Player> {
+    var playersList :ArrayList<Player> = ArrayList()
+    playersList.add(player)
+    return playersList
 }
 
+fun removePlayer(player: Player): ArrayList<Player> {
+    var playersList :ArrayList<Player> = ArrayList()
+    playersList.remove(player)
+    return playersList
+}
 open class Player(playerNumber: Int, position: Int) :MovePlayers {
     open var playerName :String = ""
-    open var playerColor  :String = ""
+    open var playerColor :String = ""
     open var isBotPlayer = false
     override fun goUp(){}
     override fun goDown() {}
 }
 
-
 class BotPlayer(playerNumber: Int, position: Int):Player(playerNumber, position) {
-    override var isBotPlayer: Boolean
-        get() = super.isBotPlayer
-        set(value) {}
+
     override var playerName: String = "BOT Player"
+    override var isBotPlayer: Boolean = false
+        get() = super.isBotPlayer
+        set(value) { field = true}
     override fun goUp(){}
     override fun goDown() {}
 }
 
 open class Snake(){
-   open val mapOfSnakes = mapOf<Int, Int>()
+//    Go Down
+   private val mapOfSnakes = mapOf<Int, Int>(16 to 6, 47 to 26, 49 to 11, 56 to 53, 62 to 19,
+    64 to 60, 87 to 24, 93 to 73, 95 to 75, 98 to 78,)
 
-    fun goDown() {}
-//go down is minus
+    open fun move(player: Player, position: Int) {
+        for (number in mapOfSnakes.keys){
+            if (position == number){
+               val newPosition = mapOfSnakes.getValue(number)
+                return move(player,newPosition)
+            }
+        }
+
+    }
 }
 class Ladder():Snake(){
+//    Go Up
 
-    override val mapOfSnakes: Map<Int, Int>
-        get() = super.mapOfSnakes
-//    val mapOfLadders = mapOf<Int, Int>()
-    fun goUp(){}
-    //go up is plus
+   private var mapOfLadders = mapOf<Int,Int>(1 to 38, 4 to 14, 9 to 31, 21 to 42,
+       28 to 84, 36 to 44, 51 to 67, 71 to 91, 80 to 100)
+    override fun move(player: Player, position: Int) {
+        for (number in mapOfLadders.keys){
+            if (position == number) {
+                val newPosition = mapOfLadders.getValue(number)
+                return move(player, newPosition)
+            }
+        }
+    }
+
 }
 
 
